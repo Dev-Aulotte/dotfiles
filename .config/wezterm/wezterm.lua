f82c56c1870d9a7854df309298ed1fe79ae4d413
NoctuaDev <noctuaapps@gmail.com>
2024-02-22 21:10:51 +1100

    Feat: Update Starship & wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,73 @@
+---@type Wezterm
 local wezterm = require("wezterm")
-return {
-	color_scheme = "tokyonight_night",
-	window_decorations = "RESIZE",
-	font = wezterm.font("VictorMono Nerd Font", { weight = "Regular" }),
-	font_size = 12.0,
-	enable_scroll_bar = false,
-	enable_tab_bar = false,
-	audible_bell = "Disabled",
+local config = wezterm.config_builder()
 
-	-- Toggle fullscreen mapping (F11)
-	keys = {
-		{ key = "F11", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
+wezterm.log_info("reloading")
+
+-- require("tabs").setup(config)
+-- require("mouse").setup(config)
+-- require("links").setup(config)
+-- require("keys").setup(config)
+
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
+-- config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+
+-- Colorscheme
+config.color_scheme = "tokyonight_night"
+
+config.underline_thickness = 3
+config.cursor_thickness = 4
+config.underline_position = -6
+
+if wezterm.target_triple:find("windows") then
+	config.default_prog = { "pwsh" }
+	config.window_decorations = "RESIZE|TITLE"
+	wezterm.on("gui-startup", function(cmd)
+		local screen = wezterm.gui.screens().active
+		local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+		local gui = window:gui_window()
+		local width = 0.7 * screen.width
+		local height = 0.7 * screen.height
+		gui:set_inner_size(width, height)
+		gui:set_position((screen.width - width) / 2, (screen.height - height) / 2)
+	end)
+else
+	config.term = "wezterm"
+	config.window_decorations = "RESIZE"
+	config.enable_tab_bar = false
+end
+
+-- Fonts
+config.font_size = 11
+config.font = wezterm.font({ family = "VictorMono Nerd Font" })
+config.bold_brightens_ansi_colors = true
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({ family = "VictorMono Nerd Font", weight = "Bold", style = "Italic" }),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = wezterm.font({ family = "VictorMono Nerd Font", weight = "DemiBold", style = "Italic" }),
+	},
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font({ family = "VictorMono Nerd Font", style = "Italic" }),
 	},
 }
+
+-- Cursor
+config.default_cursor_style = "BlinkingBar"
+config.force_reverse_video_cursor = true
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+-- window_background_opacity = 0.9,
+-- cell_width = 0.9,
+config.scrollback_lines = 10000
+
+return config --[[@as Wezterm]]
