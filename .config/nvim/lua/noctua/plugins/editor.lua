6c9a2f50a225bc594600e2f0f533eef60758593f
NoctuaDev <noctuaapps@gmail.com>
2024-03-15 08:55:17 +1100

    Feat: Nvim - Catppuccin

--- .config/nvim/lua/noctua/plugins/editor.lua
+++ .config/nvim/lua/noctua/plugins/editor.lua
@@ -175,7 +175,7 @@ return {
 			require("lualine").setup({
 				options = {
 					icons_enabled = true,
-					theme = "catppuccin",
+					theme = "auto",
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
